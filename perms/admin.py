d3d732b523966a2b287d79009308505969729793
David Isaacson <david.isaacson@ff0000.com>
2010-12-01 16:51:57 -0800

    - Changed permission checker to operate the standard way, so permissions are effectively ORed now instead of ANDed. - Added view control: if you don't have permission to modify something you won't see it in the interface.

--- perms/admin.py
+++ perms/admin.py
@@ -13,16 +13,19 @@ class GroupObjectPermissionInline(GenericTabularInline):
     model = GroupObjectPermission
 
 class ObjectPermissionMixin(admin.ModelAdmin):
+
     def has_change_permission(self, request, obj=None):
+
         opts = self.opts
         return request.user.has_perm(opts.app_label + '.' + \
                 opts.get_change_permission(), obj)
 
     def has_delete_permission(self, request, obj=None):
+
         opts = self.opts
         return request.user.has_perm(opts.app_label + '.' + \
                 opts.get_delete_permission(), obj)
-
+        
     def change_view(self, request, *args, **kwargs):
         try:
             return super(ObjectPermissionMixin, self).change_view(request, *args, **kwargs)
@@ -31,3 +34,8 @@ class ObjectPermissionMixin(admin.ModelAdmin):
             messages.add_message(request, messages.ERROR, u"You don't have permissions necessary to edit this page.")
             return HttpResponseRedirect(reverse("admin:%s_%s_changelist" % (ct.app_label, ct.model)))
 
+    def queryset(self, request):
+        qs = super(ObjectPermissionMixin, self).queryset(request)
+        have_change_perm = [obj.id for obj in qs if self.has_change_permission(request, obj)]
+        # Now comes the totally not-hacked-together part
+        return qs.filter(id__in = have_change_perm)
--- perms/backends.py
+++ perms/backends.py
@@ -1,34 +1,53 @@
+from django.core.exceptions import FieldError, ObjectDoesNotExist
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 
-from perms.models import *
+from perms.models import UserObjectPermission, GroupObjectPermission
 
 class ObjectPermissionBackend(object):
     supports_object_permissions = True
-    supports_anonymous_user = True
-
+    supports_anonymous_user = False
+    
     def authenticate(self, username, password):
         return None
+    
+    def has_module_perms(self, user_obj, app_label):
+        #TODO: actually introspect for permissions?
+        # Actual permissions are checked anyways, returning false is just a way
+        # to save time if you're sure they don't have permissions.
+        return True
 
     def has_perm(self, user_obj, perm, obj=None):
-        if obj is None:
-            return False
-
-        ct = ContentType.objects.get_for_model(obj)
+        # "If `obj` is None, this should return True if the given request has
+        #  permission to [act on] *any* object of the given type."
+       
+        if obj is not None:
+            ct = ContentType.objects.get_for_model(obj)
+        else:
+            try:
+                perm_obj = Permission.objects.get(codename = perm.split('.')[-1])
+                ct = perm_obj.content_type
+            except ObjectDoesNotExist:
+                return False
 
         try:
             perm = perm.split('.')[-1].split("_")[0]
         except IndexError:
             return False
 
-        user_perms = UserObjectPermission.objects.filter(content_type=ct,
-                object_id=obj.id, user=user_obj)
+        user_perms = UserObjectPermission.objects.filter(content_type=ct, 
+                user=user_obj)
         group_perms = GroupObjectPermission.objects.filter(content_type=ct,
-                object_id=obj.id, group__in=user_obj.groups.all())
-
-        has_user_perms = any(map(lambda k: getattr(k, 'can_%s' % perm), user_perms))
-        has_group_perms = any(map(lambda k: getattr(k, 'can_%s' % perm), group_perms))
-
-        return has_group_perms or has_user_perms
+                group__in=user_obj.groups.all())
+        
+        if obj is not None:
+            user_perms = user_perms.filter(object_id = obj.id)
+            group_perms = group_perms.filter(object_id = obj.id)
+            
+        kwargs = {'can_%s' % perm: True}
+        try:
+            return bool(group_perms.filter(**kwargs) or user_perms.filter(**kwargs))
+        except FieldError:
+            return False
 
